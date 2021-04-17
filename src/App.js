da7f7995a14a691370c9551c6238352dd1c29e00
Kord Boniadi <kordbon1@gmail.com>
2021-04-17 14:18:54 -0700

    homepage almost done

--- src/App.js
+++ src/App.js
@@ -2,40 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import NavBar from "components/NavBar";
+import HomePage from "components/HomePage"
 
 function App() {
   return (
     <div className="App">
       <div className="main-menu">
         <NavBar />
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
+        <HomePage />
       </div>
     </div>
   );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,44 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Typed from "react-typed";
+import "./HomePage.scss";
 
 const HomePage = () => {
   return (
-      
+    <div id="home">
+      <div className="content">
+        <Container>
+          <Row>
+            <Col xl={8} md={12}>
+              <div className="text-container">
+                <h1 className="title">
+                  Kord
+                  <br />
+                  Boniadi
+                </h1>
+                <h4 className="typed-text">
+                  <Typed
+                    strings={[
+                      "Coder",
+                      "Full Stack",
+                      "Programmer",
+                      "Student",
+                      "Software Engineer",
+                    ]}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    loop
+                  />
+                </h4>
+                <div className="d-flex flex-row flex-wrap">
+                  <Button type="button" variant="outline-primary" href="../assets/kord-boniadi-resume.pdf" target="_blank" className="btn button primary-button mr-4 text-uppercase">Resume</Button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 };
 
